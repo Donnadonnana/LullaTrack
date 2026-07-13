5c1c084aaa4aa86a4f0af9e59c4bdb70e715f159
Donna Huang <donnaphzj@gmail.com>
2026-07-13 11:25:46 -0400

    add buttons to test onboarding

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,41 +1,83 @@
 import { createBrowserRouter } from "react-router-dom";
+import AuthGuard from "../components/Guards/AuthGuard";
 
 import MainLayout from "../layouts";
 import DashboardPage from "../pages/DashboardPage";
 import SleepPage from "../pages/SleepPage";
 import FeedingPage from "../pages/FeedingPage";
 import NotFoundPage from "../pages/NotFoundPage";
+import LoginPage from "../pages/Auth/LoginPage";
+import RegisterPage from "../pages/Auth/RegisterPage";
+import OnboardingPage from "../pages/Onboarding/OnboardingPage";
 
 export const router: any = createBrowserRouter([
   {
-    path: "/",
-    element: <MainLayout />,
+    path: "/login",
+
+    element: <LoginPage />,
+  },
+
+  {
+    path: "/register",
+
+    element: <RegisterPage />,
+  },
+
+  {
+    element: <AuthGuard />,
+
     children: [
       {
-        index: true,
-        element: <DashboardPage />,
-        handle: {
-          title: "Dashboard",
-        },
+        path: "/onboarding",
+
+        element: <OnboardingPage />,
       },
+
       {
-        path: "sleep",
-        element: <SleepPage />,
-        handle: {
-          title: "Sleep",
-        },
-      },
-      {
-        path: "feeding",
-        element: <FeedingPage />,
-        handle: {
-          title: "Feeding",
-        },
+        path: "/",
+
+        element: <MainLayout />,
+
+        children: [
+          {
+            index: true,
+
+            element: <DashboardPage />,
+
+            handle: {
+              title: "Dashboard",
+            },
+          },
+
+          {
+            path: "sleep",
+
+            element: <SleepPage />,
+
+            handle: {
+              title: "Sleep",
+            },
+          },
+
+          {
+            path: "feeding",
+
+            element: <FeedingPage />,
+
+            handle: {
+              title: "Feeding",
+            },
+          },
+        ],
       },
     ],
   },
+
   {
     path: "*",
+
     element: <NotFoundPage />,
   },
 ]);
+
+export default router;
