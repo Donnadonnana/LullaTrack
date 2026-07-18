1821dae2894d61dcccc36aa5609af3a79acf0196
Donna Huang <donnaphzj@gmail.com>
2026-07-18 10:10:15 -0400

    add date picker to sleep log page

--- frontend/src/pages/SleepPage.tsx
+++ frontend/src/pages/SleepPage.tsx
@@ -4,20 +4,21 @@ import BedtimeOutlinedIcon from "@mui/icons-material/BedtimeOutlined";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 
+import { useState } from "react";
+import dayjs from "dayjs";
+
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { addSleepLog, type SleepType } from "../store/slices/sleepSlice";
 
 import SleepCard from "../components/SleepCard/SleepCard";
 import WakeWindow from "../components/WakeWindow/WakeWindow";
-import { calculateWakeWindow } from "../utils/time";
-
-import { useState } from "react";
-import dayjs from "dayjs";
-
 import DateNavigator from "../components/DateNavigator/DateNavigator";
+
+import { calculateWakeWindow } from "../utils/time";
 
 export default function SleepPage() {
   const dispatch = useAppDispatch();
+
   const [selectedDate, setSelectedDate] = useState(
     dayjs().format("YYYY-MM-DD"),
   );
@@ -27,12 +28,12 @@ export default function SleepPage() {
   const activeBaby = babies.find((baby) => baby.id === activeBabyId);
 
   const activeBabyLogs = useAppSelector((state) =>
-    state.sleep.logs.filter((log) => log.babyId === state.babies.activeBabyId && log.date === selectedDate),
+    state.sleep.logs.filter(
+      (log) => log.babyId === activeBabyId && log.date === selectedDate,
+    ),
   );
 
-  const hasNightSleepLog = activeBabyLogs.some(
-    (log) => log.type === "night",
-  );
+  const hasNightSleepLog = activeBabyLogs.some((log) => log.type === "night");
 
   const handleAddSleep = (type: SleepType) => {
     if (!activeBabyId) {
@@ -45,37 +46,55 @@ export default function SleepPage() {
 
     const napCount = activeBabyLogs.filter((log) => log.type === "nap").length;
 
-    const handleAddSleep = (type: SleepType) => {
-        if (!activeBabyId) {
-          return;
-        }
-      
-        if (type === "night" && hasNightSleepLog) {
-          return;
-        }
-      
-        const napCount = activeBabyLogs.filter(
-          (log) => log.type === "nap",
-        ).length;
-      
-        dispatch(
-          addSleepLog({
-            babyId: activeBabyId,
-            date: selectedDate,
-            type,
-            sleepNumber:
-              type === "night" ? 1 : napCount + 1,
-          }),
-        );
-      };
+    dispatch(
+      addSleepLog({
+        babyId: activeBabyId,
+        date: selectedDate,
+        type,
+        sleepNumber: type === "night" ? 1 : napCount + 1,
+      }),
+    );
+  };
 
   if (!activeBaby) {
     return <Typography>Select or add a baby before tracking sleep.</Typography>;
   }
 
   return (
-    
     <Stack spacing={3}>
+      <DateNavigator value={selectedDate} onChange={setSelectedDate} />
+
+      <Stack
+        direction={{
+          xs: "column",
+          sm: "row",
+        }}
+        spacing={2}
+        sx={{
+          justifyContent: "flex-end",
+        }}
+      >
+        <Button
+          variant="contained"
+          size="large"
+          startIcon={<LightModeOutlinedIcon />}
+          onClick={() => handleAddSleep("nap")}
+        >
+          Add nap
+        </Button>
+
+        {!hasNightSleepLog && (
+          <Button
+            variant="outlined"
+            size="large"
+            startIcon={<DarkModeOutlinedIcon />}
+            onClick={() => handleAddSleep("night")}
+          >
+            Add night sleep
+          </Button>
+        )}
+      </Stack>
+
       {activeBabyLogs.length === 0 ? (
         <Box
           sx={{
@@ -167,7 +186,7 @@ export default function SleepPage() {
             const previousLog = index > 0 ? activeBabyLogs[index - 1] : null;
 
             const wakeWindow = previousLog
-              ? calculateWakeWindow(previousLog.pickupTime, log.asleepTime)
+              ? calculateWakeWindow(previousLog.pickupTime, log.onBedTime)
               : null;
 
             return (
@@ -191,16 +210,14 @@ export default function SleepPage() {
               alignSelf: "flex-start",
             }}
           >
-            {!hasNightSleepLog && (
-              <Button
-                variant="outlined"
-                size="large"
-                startIcon={<LightModeOutlinedIcon />}
-                onClick={() => handleAddSleep("nap")}
-              >
-                Add another nap
-              </Button>
-            )}
+            <Button
+              variant="outlined"
+              size="large"
+              startIcon={<LightModeOutlinedIcon />}
+              onClick={() => handleAddSleep("nap")}
+            >
+              Add another nap
+            </Button>
 
             {!hasNightSleepLog && (
               <Button
